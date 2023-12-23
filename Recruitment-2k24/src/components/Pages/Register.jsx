5e4bc248c8f42a4cb547bd7502446e15fb1d3b2e
Aryan Kushwaha <aryankushwaha3101@gmail.com>
2023-12-23 20:37:11 +0530

    fixed fonts

--- Recruitment-2k24/src/components/Pages/Register.jsx
+++ Recruitment-2k24/src/components/Pages/Register.jsx
@@ -1,4 +1,4 @@
-import Navbar from "../Navbar";
+import Navbar from "../navbar";
 import RegistrationForm from "../RegistrationForm";
 import SelectorButton from "../SelectorButton";
 export default function Register(){
@@ -6,7 +6,7 @@ export default function Register(){
     <>
     <Navbar btnName={"Home"}/>
     <div className="bg-dark-background h-[90vh]">
-        <h1 className="text-8xl text-center font-Zodiac text-header font-bold">Let's get started!</h1>
+        <h1 className="text-8xl text-center font-Zodiac text-header font-extrabold">Let's get started!</h1>
         <SelectorButton/>
         <RegistrationForm />
     </div>
--- Recruitment-2k24/src/components/navbar.jsx
+++ Recruitment-2k24/src/components/navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = ({btnName}) => {
         <>
             <div className="bg-dark-blue w-screen m-0 h-[10vh] flex items-center justify-between">
                 <img className="h-10 ml-20" src={gdsc_logo} alt="gdsc_logo" />
-                <button className="text-green-light border-[3px] border-green-dark rounded-lg px-8 py-2 bg-green-bg mr-20 font-Satoshi font-medium ease-in-out duration-200 hover:bg-green-950">{btnName}</button>
+                <button className="text-green-light border-[3px] border-green-dark rounded-lg px-8 py-2 bg-green-bg mr-20 font-Satoshi font-bold ease-in-out duration-200 hover:bg-green-950">{btnName}</button>
             </div>
         </>
     );
